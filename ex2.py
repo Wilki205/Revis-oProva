803964dd3120ea3295dc116d1847685f7c35885f
will <wilkisonsouza167@gmail.com>
2025-04-17 09:36:07 -0300

    atualização do ex3 para verificar se o usuario deseja continuar ou não

--- ex2.py
+++ ex2.py
@@ -1,17 +1,19 @@
-n = int(input("Digite um numero: "))
-cont = 1, 2
-div = n % 2
+rep = "s"
+while rep == "s":
+    n = int(input("Digite um numero: "))
+    div = n % 2
+    if n > 0:
+        if n%2==0:
+            print ("Par e Positivo")
+        else:
+            print ("Impar e Positivo")
+    else:
+        if n%2==0:
+         print ("Par e Negativo")
+        else:
+            print ("Impar e Negativo")
+    rep = input("Digite s para continuar!")
 
-if n > 0:
-    if n%2==0:
-        print ("Par e Positivo")
-    else:
-        print ("Impar e Positivo")
-else:
-    if n%2==0:
-     print ("Par e Negativo")
-    else:
-        print ("Impar e Negativo")
 
 
 
