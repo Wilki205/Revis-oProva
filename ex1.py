55df7aeeae57d2e6d5cecaaa5ce79ce9829048c0
will <wilkisonsouza167@gmail.com>
2025-04-17 08:33:55 -0300

    ex1... receber 3 valores e somar a+b e dizer se a soma de a+b é menor que c

---
+++ ex1.py
@@ -0,0 +1,10 @@
+a = int(input("Digite um valor: "))
+b = int(input("Digite um valor: "))
+c = int(input("Digite um valor: "))
+
+soma = a+b
+
+if soma < c:
+    print (f"a soma é menor que {c}")
+else:
+        print (soma)
