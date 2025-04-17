ee323a62d1b90781ec9c54fee94487a0a73342c1
will <wilkisonsouza167@gmail.com>
2025-04-17 11:06:42 -0300

    expressões simplificadas

--- ex5.py
+++ ex5.py
@@ -4,13 +4,13 @@ imc = peso / (altura*altura)
 print (f"{imc:.2f}")
 if imc < 18.5:
     print ("Abaixo do peso!")
-elif imc >18.5 and imc < 24.9:
+elif 18.5 < imc < 24.9:
     print ("Peso ideal, Parabens")
-elif imc >25.0 and imc < 29.9:
+elif 25.0 < imc < 29.9:
     print ("Levemente acima do peso")
-elif imc >30.0 and imc < 34.9:
+elif 30.0 < imc < 34.9:
     print ("obesidade")
-elif imc >35.0 and imc < 39.9:
+elif 35.0 < imc < 39.9:
     print ("obesidade severa")
 elif imc > 40.0:
     print("Obsidade morbida")
