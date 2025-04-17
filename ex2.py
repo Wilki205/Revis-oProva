38abf20345fed3ed7ff42c7453124df808b6512c
will <wilkisonsouza167@gmail.com>
2025-04-17 09:14:02 -0300

    ex3 verificar se o numero é par ou impar e se é par negativo ou positivo e se impar negativo ou positivo

--- ex2.py
+++ ex2.py
@@ -1,8 +1,19 @@
 n = int(input("Digite um numero: "))
-
+cont = 1, 2
 div = n % 2
 
-if div <= 0:
-    print ("par")
+if n > 0:
+    if n%2==0:
+        print ("Par e Positivo")
+    else:
+        print ("Impar e Positivo")
 else:
-    print ("impar")
+    if n%2==0:
+     print ("Par e Negativo")
+    else:
+        print ("Impar e Negativo")
+
+
+
+
+
